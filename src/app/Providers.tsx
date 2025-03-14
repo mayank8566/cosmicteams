8bb40e2a22acc713ed239eea1549b574b827a262
mayank8566 <mayank84379@gmail.com>
2025-03-14 13:21:40 +0530

    fix2

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { useEffect } from 'react';
-import { AuthProvider } from '@/lib/AuthContext';
-import { ProfileProvider } from '@/lib/ProfileContext';
-import { preloadCommonPages, optimizeAuthProcess } from '@/lib/clientUtils';
+import { AuthProvider } from '../lib/AuthContext';
+import { ProfileProvider } from '../lib/ProfileContext';
+import { preloadCommonPages, optimizeAuthProcess } from '../lib/clientUtils';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   useEffect(() => {
