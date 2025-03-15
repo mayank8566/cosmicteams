f24c9d947717bbd4d4105c7d353bf0968d8780d2
mayank8566 <mayank84379@gmail.com>
2025-03-15 09:35:15 +0530

    fix3

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"asnDB42CS9sc6jX2bxiEMhXucrdrLaNrSwdocZ88TjM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
